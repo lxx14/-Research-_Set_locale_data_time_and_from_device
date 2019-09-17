f5d17930f939f47586cd54730f2d4f59157d9b0a
AlexeyIlchenko <alexeyi@wizardsdev.com>
2019-09-17 10:20:05 +0300

    resole research

--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+import moment from "moment/min/moment-with-locales";
 
-import { styles } from './styles';
+import {styles} from './styles';
+
 
 export default class OpenGL extends Component {
+
+
+
     render() {
+        const deviceCountry = DeviceInfo.getDeviceCountry().toLowerCase(); // get country (recieved from Language preferences in device settings)
+
+        const is24Hour = DeviceInfo.is24Hour();  // get time format from device. If in device settings switcher stay in 24 hour position, this method returns "true"
+
         return (
             <View style={styles.container}>
-                <Text>Screen</Text>
+                {
+                    is24Hour ?
+                        <Text>time EUR: {moment().format("HH:mm")}</Text> :  // 24 H format 
+                        <Text>time US: {moment().format("hh:mm a")}</Text>   // 12 H format
+                }
+                <Text>{moment.locale('us') /* put localization for moment */}</Text>
+                <Text>{moment().format("l") /*get date in device country format. You can change "l" to "ll" for other output style of date */}</Text>
             </View>
         );
     }
